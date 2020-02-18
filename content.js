e5a68176bdea9780b6425cba50b6133f7e1dc55c
jiwoochoi <pelomon2@gmail.com>
2020-02-18 16:27:40 -0600

    - storage permission 추가 - logic 저장하고 꺼내오는 로직 추가.

--- content.js
+++ content.js
@@ -1,14 +1,12 @@
-/**
- * @author Jiwoo, Choi
- * @brief HTML태그 중 게시글과 관련된 태그를 찾고 바꿔주는 스크립트.
- */
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	const titleTag = document.querySelectorAll("h3.title");
-  	for(let element of titleTag) {
-  		if(element.innerText.includes(request.text)) {
-			element.offsetParent.style.display="none";
-  		}
-  	}
-});
+	function(request, sender, sendResponse) {
+		const titleTag = document.querySelectorAll("h3.title");
+		for(let element of titleTag) {
+			if(element.innerText.includes(request.text)) {
+				element.offsetParent.style.display="none";
+			}
+		}
+	});
+	
+
--- popup.js
+++ popup.js
@@ -1,8 +1,56 @@
-const sendButton = document.querySelector("#sendBtn");
-const input = document.querySelector("#textArea");
-sendButton.addEventListener("click", () => {
-	alert(input.value)
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		 chrome.tabs.sendMessage(tabs[0].id, {text: input.value});
+
+//when loaded this should be called.
+function viewDidLoad(){
+	updateChipUI()
+	const sendButton = document.querySelector("#sendBtn");
+	const input = document.querySelector("#textArea");
+
+	sendButton.addEventListener("click", () => {
+
+		/*
+		chrome.storage.sync.set({'keyword': 'av'}, function() {
+			// Notify that we saved.
+			//if null?
+		  });*/
+		saveKeyword(input.value);
+
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {text: input.value}); //
+		});
+
 	});
-});
+
+}
+
+function reteriveCurrentKeywordList(){
+	return new Promise(function(resolve) {
+		chrome.storage.sync.get(['keyword'], function(result) {
+			let currentKeywords;
+			currentKeywords = result.keyword
+			currentKeywords = (currentKeywords == undefined) ? [] : currentKeywords; // avoid undefined case.
+			resolve(currentKeywords)
+		});
+	})
+}
+
+async function saveKeyword(key){
+	if (key !== "") {
+		let currentKeywords = await reteriveCurrentKeywordList()
+		currentKeywords.push(key)   
+		alert(currentKeywords)
+		chrome.storage.sync.set({'keyword': currentKeywords}, function() { 
+			updateChipUI();
+		});	
+	}
+}
+
+async function updateChipUI(){	
+	const keywordLists = await reteriveCurrentKeywordList();
+	alert(keywordLists)
+}
+
+document.addEventListener("DOMContentLoaded", function(){viewDidLoad()}, false);
+
+
+
+
