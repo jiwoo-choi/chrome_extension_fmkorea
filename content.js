fb823f0e9030936abf66e56349443575fb063d9a
litain <litain23@gmail.com>
2020-02-18 19:35:08 +0900

    메세지 방식으로 변경
    send 버튼을 누를 시, 해당 내용과 같은 제목이 사라짐

--- content.js
+++ content.js
@@ -3,11 +3,12 @@
  * @brief HTML태그 중 게시글과 관련된 태그를 찾고 바꿔주는 스크립트.
  */
 
-var titleTag = document.getElementsByClassName('title')
-var titleArray = Array.from(titleTag)
-var filteredArray = titleArray.filter(element=>element.tagName == "H3")
-filteredArray.forEach(element => {
-    if (element.innerText.includes("권상훈")) {
-        element.offsetParent.style.display="None"
-    }
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	const titleTag = document.querySelectorAll("h3.title");
+  	for(let element of titleTag) {
+  		if(element.innerText.includes(request.text)) {
+			element.offsetParent.style.display="none";
+  		}
+  	}
 });
