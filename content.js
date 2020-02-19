a56eed6f13498c12884693d670f65781d6296d03
litain <litain23@gmail.com>
2020-02-19 16:06:51 +0900

    Storage 에 저장된 keyword 및 추가되는 데이터 삭제
    현재 extension 아이콘을 눌러야 viewDidLoad 함수 콜
    => 페이지에 들어가자마자 함수가 동작하도록 변경 필요

--- content.js
+++ content.js
@@ -1,10 +1,15 @@
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
+		const keywordList = request.keyword;
+
+		console.log(keywordList);
 		const titleTag = document.querySelectorAll("h3.title");
 		for(let element of titleTag) {
-			if(element.innerText.includes(request.text)) {
-				element.offsetParent.style.display="none";
+			for(const keyword of keywordList) {
+				if(element.innerText.includes(keyword)) {
+					element.offsetParent.style.display = "none";
+				}
 			}
 		}
 	});
--- popup.js
+++ popup.js
@@ -1,52 +1,54 @@
 
 //when loaded this should be called.
 function viewDidLoad(){
-	updateChipUI()
+	updateChipUI().then(keywordList => {
+		// stoarge 에 있던 데이터 삭제
+		sendKeyword(keywordList);
+	});
+
 	const sendButton = document.querySelector("#sendBtn");
 	const input = document.querySelector("#textArea");
 
 	sendButton.addEventListener("click", () => {
-
-		/*
-		chrome.storage.sync.set({'keyword': 'av'}, function() {
-			// Notify that we saved.
-			//if null?
-		  });*/
-		saveKeyword(input.value);
-
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, {text: input.value}); //
+		// 추가 후 삭제
+		saveKeyword(input.value).then(keywordList => {
+			sendKeyword(keywordList);
 		});
-
 	});
+}
 
+function sendKeyword(keywordList) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {keyword: keywordList}); //
+	});
 }
 
 function reteriveCurrentKeywordList(){
 	return new Promise(function(resolve) {
 		chrome.storage.sync.get(['keyword'], function(result) {
-			let currentKeywords;
-			currentKeywords = result.keyword
-			currentKeywords = (currentKeywords == undefined) ? [] : currentKeywords; // avoid undefined case.
-			resolve(currentKeywords)
+			let currentKeywordList;
+			currentKeywordList = result.keyword
+			currentKeywordList = (currentKeywordList == undefined) ? [] : currentKeywordList; // avoid undefined case.
+			resolve(currentKeywordList)
 		});
 	})
 }
 
 async function saveKeyword(key){
 	if (key !== "") {
-		let currentKeywords = await reteriveCurrentKeywordList()
-		currentKeywords.push(key)   
-		alert(currentKeywords)
-		chrome.storage.sync.set({'keyword': currentKeywords}, function() { 
+		let currentKeywordList = await reteriveCurrentKeywordList()
+		currentKeywordList.push(key)   
+		alert(currentKeywordList)
+		chrome.storage.sync.set({'keyword': currentKeywordList}, function() { 
 			updateChipUI();
 		});	
+		return currentKeywordList;
 	}
 }
 
 async function updateChipUI(){	
-	const keywordLists = await reteriveCurrentKeywordList();
-	alert(keywordLists)
+	const keywordList = await reteriveCurrentKeywordList();
+	return keywordList;
 }
 
 document.addEventListener("DOMContentLoaded", function(){viewDidLoad()}, false);
