fcfa65b064514acfadb7201ff6a35c7b980cae30
jiwoochoi <pelomon2@gmail.com>
2020-02-19 01:43:11 -0600

    - class형태의 Storage Control

---
+++ StorageControl.js
@@ -0,0 +1,31 @@
+class StorageControl {
+    
+    constructor(keyword) {
+        this.keyword = keyword
+    }
+
+    async getKeys() {
+        return new Promise(function(resolve) {
+            chrome.storage.sync.get([this.keyword], function(result) {
+                let currentKeywords;
+                currentKeywords = result.keyword
+                currentKeywords = (currentKeywords == undefined) ? [] : currentKeywords; // avoid undefined case.
+                resolve(currentKeywords)
+            });
+        })
+    }
+
+    async saveKey(key) {
+        return new Promise(async function(resolve) {
+            if (key !== "") {
+                let currentKeywords = await getKeys()
+                currentKeywords.push(key)
+                dictTemp[this.keyword] = currentKeywords
+                chrome.storage.sync.set(dictTemp, function() { 
+                    resolve(true)
+                });
+            }
+        })
+    }
+}
+
