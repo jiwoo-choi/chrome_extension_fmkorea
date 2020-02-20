a68864ebd04f944d1c7a552a7bbbd4a89386e6a0
litain <litain23@gmail.com>
2020-02-20 21:22:03 +0900

    content js 를 단지 main 를 부르기 위해 수정
    모든 로직은 main js 에 있음

--- content.js
+++ content.js
@@ -1,36 +1,5 @@
-async function updateKeywords() {
-	return new Promise(function(resolve){
-			chrome.storage.sync.get(['keyword'], function(result) {
-				let currentKeywords;
-				currentKeywords = result.keyword
-				currentKeywords = (currentKeywords == undefined) ? [] : currentKeywords; // avoid undefined case.
-				resolve(currentKeywords);
-			});		
-	})
-}
-
-async function removeTitles(keywordList) {
-	const titleTag = document.querySelectorAll("h3.title");
-	for(let element of titleTag) {
-		for(const keyword of keywordList) {
-			if(element.innerText.includes(keyword)) {
-				element.offsetParent.style.display = "none";
-			}
-		}
-	}
-}
-
-chrome.runtime.onMessage.addListener(
-	async function(request, sender, sendResponse) {
-		const keywordList = await updateKeywords();
-		await removeTitles(keywordList);
-});
-
-
- async function viewDidLoad(){
- 	const keywordList = await updateKeywords();
-	await removeTitles(keywordList);
-}
-viewDidLoad();
-
-//viewDidLoad();
+(async() => {
+	const src = chrome.extension.getURL('main.js');
+	const contentScript = await import(src);
+	contentScript.main();
+})();
---
+++ main.js
@@ -0,0 +1,30 @@
+import StorageController from './StorageController.js';
+
+function removeTitles(keywordList) {
+	const titleTag = document.querySelectorAll("h3.title");
+	for(let element of titleTag) {
+		for(const keyword of keywordList) {
+			if(element.innerText.includes(keyword)) {
+				element.offsetParent.style.display = "none";
+			}
+		}
+	}
+}
+
+export function main() {
+	const storageController = new StorageController("keyword");
+
+	async function filterTitle() {
+		storageController.getKeywordList().then(keywordList => {
+			removeTitles(keywordList);	
+		})
+	}
+
+	filterTitle();
+
+	chrome.runtime.onMessage.addListener(
+		async function(request, sender, sendResponse) {
+			filterTitle();
+	});
+}
+
