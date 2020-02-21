862bf2e40c37ae951ffcfc3cfc7fa1a96e74aac1
jiwoochoi <pelomon2@gmail.com>
2020-02-20 22:50:19 -0600

    20202020 send 버튼 누르면 chip이 되도록 update. - popup.html: chipArea <div> 추가. - popup.js: addChip() 을 통해 html tag injection. - style.jss : mateiral style chip관련 css추가 (codepen출처)

--- popup.js
+++ popup.js
@@ -1,7 +1,18 @@
 const sendButton = document.querySelector("#sendBtn");
 const input = document.querySelector("#textArea");
+
+
+
+const addChip = (text) => {
+	let tag = "<div class='md-chip'> <span>" + text + "</span> <button type='button' class='md-chip-remove'></button></div>"
+	let inputArea = document.querySelector("#chipArea");
+	inputArea.innerHTML = inputArea.innerHTML + tag;
+}
+
 sendButton.addEventListener("click", () => {
-	alert(input.value)
+	addChip(input.value);
+	input.value = "";
+
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		 chrome.tabs.sendMessage(tabs[0].id, {text: input.value});
 	});
